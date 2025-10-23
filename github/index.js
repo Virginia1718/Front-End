42bc300a4623ef96804e8b4bdc19098971897fc8
Virginia Rugian <12410231@student.unklab.ac.id>
2025-10-23 13:17:20 +0800

    adding pertemuan 16

--- github/index.js
+++ github/index.js
@@ -1,3 +1,6 @@
 console.log("first commit")
 console.log("latihan github")
 console.log("latihan")
+console.log("pertemuan 16")
+//konflik pertana terjadi ketika 22 mengedit di satu file
+//console.log ("fiture") // konflik2B
